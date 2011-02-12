3971cc69007707fa4ebf12e21a00af863909c306
Preston Guillory <pguillory@gmail.com>
2011-02-12 00:51:05 -0600

    Narcissus is loaded in a closure and exposed as a module, rather than being required directly.  This prevents it from creating a 'Narcissus' global variable.

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-require("../narcissus/lib/jsdefs");
-require("../narcissus/lib/jslex");
-require("../narcissus/lib/jsparse");
-require("../narcissus/lib/jsdecomp");
-
 exports.register = require("./lib/register");
 exports.transform = require("./lib/transform").transform;
 exports.flows = require("./lib/flows");
