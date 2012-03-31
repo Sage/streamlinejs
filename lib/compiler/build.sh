8030598c9152305ed20a85acb7253518001217fc
Bruno Jouhier <bjouhier@gmail.com>
2012-03-31 14:22:34 +0200

    recompiled

--- lib/compiler/build.sh
+++ lib/compiler/build.sh
@@ -4,6 +4,8 @@ mv builtins.js ../callbacks
 _node -lp -v -f --fibers -c builtins._js
 mv builtins.js ../fibers
 
+_node -lp -v -f -c compile._js
+
 # compile test files for client too
 pushd ../../test/common > /dev/null
 _node -lp -v -f -c .
