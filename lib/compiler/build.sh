8caab90dff3e976725a37eb46baca28875c6dacb
Bruno Jouhier <bjouhier@gmail.com>
2012-05-06 17:48:04 +0200

    first pass on generators with luvmonkey

--- lib/compiler/build.sh
+++ lib/compiler/build.sh
@@ -5,6 +5,10 @@ _node -lp -v -f --fibers -c builtins._js
 cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../fibers/builtins.js
 rm builtins.js
 
+_node -lp -v -f --generators -c builtins._js
+cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../generators/builtins.js
+rm builtins.js
+
 _node -lp -v -f -c compile._js
 
 # compile test files for client too
---
+++ lib/generators/proto.sh
@@ -0,0 +1,1 @@
+../../deps/luvmonkey/bin/luvmonkey test.js
