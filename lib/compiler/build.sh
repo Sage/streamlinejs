e47db695f14face89446481b57ed5c4edb25baad
Bruno Jouhier <bjouhier@gmail.com>
2012-06-15 15:04:42 +0200

    bumped fibers transform version after #108 fix (+ fixed build.sh script)

--- lib/compiler/build.sh
+++ lib/compiler/build.sh
@@ -1,21 +1,21 @@
-_node -lp -v -f -c builtins._js
+../../bin/_node -lp -v -f -c builtins._js
 mv builtins.js ../callbacks
 
-_node -lp -v -f --fibers -c builtins._js
+../../bin/_node -lp -v -f --fibers -c builtins._js
 cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../fibers/builtins.js
 rm builtins.js
 
-_node -lp -v -f --generators -c builtins._js
+../../bin/_node -lp -v -f --generators -c builtins._js
 cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../generators/builtins.js
 rm builtins.js
 
-_node -lp -v -f -c compile._js
-_node -lp -v -f -c ../streams/client/streams._js
+../../bin/_node -lp -v -f -c compile._js
+../../bin/_node -lp -v -f -c ../streams/client/streams._js
 
 # compile test files for client too
 pushd ../../test/common > /dev/null
-_node -lp -v -f -c .
+../../bin/_node -lp -v -f -c .
 mv eval-test.js flows-test.js stack-test.js callbacks
-_node --generators -v -f -c .
+../../bin/_node --generators -v -f -c .
 mv eval-test.js flows-test.js stack-test.js generators
 popd > /dev/null
