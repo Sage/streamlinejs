109914ab87d841d4a1426514dd650a7488cee0bf
Bruno Jouhier <bjouhier@gmail.com>
2012-10-06 19:58:26 +0200

    array builtins for TypeScript

--- typescript/streamline-module.d.ts
+++ typescript/streamline-module.d.ts
@@ -14,8 +14,17 @@ declare function future_(f: any) : future;
 declare function streamline() : void;
 
 interface Array_ {
-	reduce_(_: async, fn: (_: async, val: any, elt: any) => any, start: any) : any;	
+	forEach_(_: async, fn: (_: async, elt: _element, i: number) => any) : void;	
+	map_(_: async, fn: (_: async, elt: _element, i: number) => void) : any[];	
+	filter_(_: async, fn: (_: async, _element: any, i: number) => bool) : _element[];	
+	every_(_: async, fn: (_: async, val: _element, elt: any, i: number) => bool) : bool;	
+	some_(_: async, fn: (_: async, val: _element, elt: any, i: number) => bool) : bool;	
+	reduce_(_: async, fn: (_: async, val: _element, elt: any, i: number) => any, start: any) : any;	
+	reduceRight_(_: async, fn: (_: async, val: _element, elt: any, i: number) => any, start: any) : any;	
+	sort_(_: async, compare: (_: async, v1: _element, v2: _element) => number, beg: number, end: number) : _element[];	
 }
+
+// TODO: apply_
 
 declare function array_(a: any[]) : Array_;
 
