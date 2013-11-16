12767d28b4848c8d6377934757047685bd1a0150
Bruno Jouhier <bjouhier@gmail.com>
2013-11-16 22:13:30 +0100

    fixed #174 (second attempt)

--- lib/compiler/build.sh
+++ lib/compiler/build.sh
@@ -1,51 +1,33 @@
 pushd `dirname $0` > /dev/null
-../../bin/_node -lp -v -f -c builtins._js
+../../bin/_node -lp -v -f -c builtins._js flows._js
 mv builtins.js ../callbacks
+mv flows.js ../callbacks
 
 ../../bin/_node -lp -v -f -c compile._js
 cat compile.js | sed -e "s/\/\/\/ \!doc//" > ../callbacks/compile.js
 rm compile.js
 
-../../bin/_node -lp -v -f --fibers -c builtins._js
+../../bin/_node -lp -v -f --fibers -c builtins._js flows._js
 cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../fibers/builtins.js
-rm builtins.js
+cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../fibers/flows.js
+rm builtins.js flows.js
 
-../../bin/_node -lp -v -f --fibers --fast -c builtins._js
+../../bin/_node -lp -v -f --fibers --fast -c builtins._js flows._js
 cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../fibers-fast/builtins.js
-rm builtins.js
+cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../fibers-fast/flows.js
+rm builtins.js flows.js
 
-../../bin/_node -lp -v -f --generators -c builtins._js
+../../bin/_node -lp -v -f --generators -c builtins._js flows._js
 cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../generators/builtins.js
-rm builtins.js
+cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../generators/flows.js
+rm builtins.js flows.js
 
-../../bin/_node -lp -v -f --generators --fast -c builtins._js
+../../bin/_node -lp -v -f --generators --fast -c builtins._js flows._js
 cat builtins.js | sed -e "s/\/\/\/ \!doc//" > ../generators-fast/builtins.js
-rm builtins.js
+cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../generators-fast/flows.js
+rm builtins.js flows.js
 
 ../../bin/_node -lp -v -f -c ../streams/client/streams._js
-
-pushd ../util > /dev/null
-
-../../bin/_node -lp -v -f -c flows._js
-mv flows.js ../callbacks
-
-../../bin/_node -lp -v -f --fibers -c flows._js
-cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../fibers/flows.js
-rm flows.js
-
-../../bin/_node -lp -v -f --fibers --fast -c flows._js
-cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../fibers-fast/flows.js
-rm flows.js
-
-../../bin/_node -lp -v -f --generators -c flows._js
-cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../generators/flows.js
-rm flows.js
-
-../../bin/_node -lp -v -f --generators --fast -c flows._js
-cat flows.js | sed -e "s/\/\/\/ \!doc//" > ../generators-fast/flows.js
-rm flows.js
-
-popd > /dev/null
 
 # compile test files for client too
 pushd ../../test/common > /dev/null
