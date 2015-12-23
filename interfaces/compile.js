a8d07608be79bf97366cb4b3699404cc74cddce7
Bruno Jouhier <bjouhier@gmail.com>
2015-12-23 14:36:03 +0100

    issue #314 - implemented --flow option + added flow directives in all source files

--- interfaces/compile.js
+++ interfaces/compile.js
@@ -1,11 +1,35 @@
 type StreamlineOptions = {
-	filename: string;
-	outputDir: string;
-	sourceRoot: string;
+	runtime: string;
+	cache: boolean;
+	quiet: boolean;
+	filename?: string;
+	outDir?: string;
+	sourceRoot?: string;
+	sourceMaps?: boolean;
+	sourceMapTarget?: string;
+	babel?: any;
+	args?: string[];
+};
+
+type StreamlineApiOptions = any;
+
+type BabelOptions = {
+	plugins: any[];
+	presets: string[];
+	sourceMaps: boolean;
+	filename?: string;
+	sourceFileName?: string;
 };
 
 type TransformResult = {
 	code: string,
 	ast?: any,
 	map?: any, 
+};
+
+declare module "module" {
+	declare var prototype: any;
+	declare function _nodeModulePaths(path: string) : string[];
+	declare function _resolveFilename(name: string) : string;
+	declare function _findPath(name: string, paths: string[]) : string;
 };
